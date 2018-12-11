538efbec901269d925c851583a11d1294d765ae1
Augusto Calaca <augusto@Calaca.Augusto>
2018-12-11 06:11:54 -0300

    add build

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/augusto/Augusto/Documentos/Codigos/simple-parser/lexicon.cpp" "/media/augusto/Augusto/Documentos/Codigos/simple-parser/build/CMakeFiles/main.dir/lexicon.cpp.o"
+  "/media/augusto/Augusto/Documentos/Codigos/simple-parser/main.cpp" "/media/augusto/Augusto/Documentos/Codigos/simple-parser/build/CMakeFiles/main.dir/main.cpp.o"
+  "/media/augusto/Augusto/Documentos/Codigos/simple-parser/parser.cpp" "/media/augusto/Augusto/Documentos/Codigos/simple-parser/build/CMakeFiles/main.dir/parser.cpp.o"
+  "/media/augusto/Augusto/Documentos/Codigos/simple-parser/token.cpp" "/media/augusto/Augusto/Documentos/Codigos/simple-parser/build/CMakeFiles/main.dir/token.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/parser.cpp.o"
+  "CMakeFiles/main.dir/lexicon.cpp.o"
+  "CMakeFiles/main.dir/token.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
